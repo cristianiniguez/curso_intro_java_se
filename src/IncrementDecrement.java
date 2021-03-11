53cc60921511f18f61ba97b5e9f48803652bfd78
cristianiniguez <felizpc1@gmail.com>
2021-03-11 19:12:01 -0400

    trabajar con variables: operaciones de asignacion, incremento y decremento

---
+++ src/IncrementDecrement.java
@@ -0,0 +1,27 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int lives = 5;
+        lives = lives - 1;
+
+        System.out.println(lives); // 4
+
+        // Postfija
+
+        lives--; // Decremento
+        System.out.println(lives); // 3
+
+        lives++; // Incremento
+        System.out.println(lives); // 4
+
+        // Prefija
+
+        // Gana un regalo por ganar una vida
+        int gift = 100 + lives++; // postfijo
+        System.out.println(gift); // 104 (no 105)
+        System.out.println(lives); // 5
+
+        int gift2 = 100 + ++lives; // prefijo
+        System.out.println(gift2); // 106
+        System.out.println(lives); // 6
+    }
+}
