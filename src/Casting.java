4767f9898f1a93f673f47071775adfa8167a41f3
cristianiniguez <felizpc1@gmail.com>
2021-03-11 21:11:15 -0400

    trabajar con variables: casteo entre tipos de datos

--- src/Casting.java
+++ src/Casting.java
@@ -19,5 +19,20 @@ public class Casting {
 
         System.out.println(a / b); // 2 (no 2.5, porque son datos int)
         System.out.println((double) a / b); // 2.5 (ahora si)
+
+        // Casting implicito vs explicito
+        double c = a / b;
+        System.out.println(c); // 2.0 (no 2.5, por lo mismo de antes, el .0 es porque c se convierte a double solito)
+        double c2 = (double) a / b;
+        System.out.println(c2); // 2.5 (ahora si)
+
+        // Otros tipos de casting
+        char n = '1';
+        int nI = n;
+        System.out.println(nI); // 49 ('1' en la tabla ASCII)
+
+        // short nS = n; // no se puede
+        short nS = (short) n; // si se puede
+        System.out.println(nS); // 49
     }
 }
