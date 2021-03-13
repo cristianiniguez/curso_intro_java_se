2bf85ab8eebfe30c58ffaa7a382816883b1ae5c6
cristianiniguez <felizpc1@gmail.com>
2021-03-13 16:00:21 -0400

    aplicar condicionales: alcance de las variables y sentencia else

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -7,6 +7,16 @@ public class IfStatement {
             // send file
             fileSended++;
             System.out.println("Archivo enviado");
+            int i = 0;
+            i++;
+        } else {
+            fileSended--;
+            System.out.println("Por favor enciende el bluetooth para inciar la transferencia");
         }
+
+        System.out.println(isBluetoothEnabled); // false
+        System.out.println(fileSended); // 2
+
+        // System.out.println(i); // ERROR!
     }
 }
