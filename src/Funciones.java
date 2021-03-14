ec1b5d0f756558abf427174af13d14d9a14bbdd9
cristianiniguez <felizpc1@gmail.com>
2021-03-14 16:12:24 -0400

    escribir funciones: implementa funciones

---
+++ src/Funciones.java
@@ -0,0 +1,45 @@
+public class Funciones {
+    public static void main(String[] args) {
+        double y = 3;
+
+        // area de un circulo
+        // pi * r^2
+        System.out.println(circleArea(y)); // 28.27...
+
+        // area de una esfera
+        // 4 * pi * r^2
+        System.out.println(sphereArea(y)); // 113.09...
+
+        // volumen de una esfera
+        // (4/3) * pi * r^3
+        System.out.println(sphereVolume(y)); // 84.82...
+
+        System.out.println("200 PESOS A DOLARES: " + convertToDolar(200, "MXN"));
+        System.out.println("1000 PESOS A DOLARES: " + convertToDolar(200, "COP"));
+    }
+
+    public static double circleArea(double r) {
+        return Math.PI * Math.pow(r, 2);
+    }
+
+    public static double sphereArea(double r) {
+        return 4 * Math.PI * Math.pow(r, 2);
+    }
+
+    public static double sphereVolume(double r) {
+        return (4/3) * Math.PI * Math.pow(r, 3);
+    }
+
+    public static double convertToDolar(double quantity, String currency) {
+        switch (currency) {
+            case "MXN":
+                quantity = quantity * 0.052;
+                break;
+            case "COP":
+                quantity = quantity * 0.00031;
+                break;
+        }
+
+        return quantity;
+    }
+}
