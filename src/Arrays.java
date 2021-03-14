0e0408403f999d9ba0893b281306c3d58f80987b
cristianiniguez <felizpc1@gmail.com>
2021-03-14 18:34:50 -0400

    analizar e implementar ciclos: ciclos for anidados

--- src/Arrays.java
+++ src/Arrays.java
@@ -2,7 +2,7 @@ public class Arrays {
     public static void main(String[] args) {
         // Declarando un array
         String[] androidVersions = new String[17];
-        String days[] = new String[7];
+        int days[] = new int[7];
 
         // Array bidimensional
         String[][] cities = new String[4][2]; // 4 * 2 = 8 espacios
@@ -16,10 +16,24 @@ public class Arrays {
         androidVersions[2] = "Cupcake";
         androidVersions[3] = "Donut";
 
+        /*
+        *
+        for (int i = 0; i < androidVersions.length; i++) {
+            System.out.println(androidVersions[i]);
+        }
+         */
+
+        for (String androidVersion : androidVersions) {
+            System.out.println(androidVersion);
+        }
+
+        /*
+        *
         System.out.println(androidVersions[0]); // Apple Pie
         System.out.println(androidVersions[1]); // Banana Bread
         System.out.println(androidVersions[2]); // Cupcake
         System.out.println(androidVersions[3]); // Donut
+        */
 
         System.out.println("---------------------------");
 
@@ -32,6 +46,8 @@ public class Arrays {
         cities[3][0] = "México";
         cities[3][1] = "CDMX";
 
+        /*
+        *
         System.out.println(cities[0][0]); // Colombia
         System.out.println(cities[0][1]); // Medellin
         System.out.println(cities[1][0]); // Colombia
@@ -40,6 +56,22 @@ public class Arrays {
         System.out.println(cities[2][1]); // Guadalajara
         System.out.println(cities[3][0]); // México
         System.out.println(cities[3][1]); // CDMX
+        */
+
+        /*
+        *
+        for (int i = 0; i < cities.length; i++) {
+            for (int j = 0; j < cities[i].length; j++) {
+                System.out.println(cities[i][j]);
+            }
+        }
+        */
+
+        for (String[] pair : cities) {
+            for (String name : pair) {
+                System.out.println(name);
+            }
+        }
 
         String[][][][] animals = new String[2][3][2][2];
         animals[1][0][0][1] = "Monkey";
