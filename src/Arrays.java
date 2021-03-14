d0c9b87a6102661d5818887941a417f6f5e89e47
cristianiniguez <felizpc1@gmail.com>
2021-03-14 18:04:04 -0400

    analizar e implementar ciclos: declarando arreglos

---
+++ src/Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // Declarando un array
+        String[] androidVersions = new String[17];
+        String days[] = new String[7];
+
+        // Array bidimensional
+        String[][] cities = new String[4][2]; // 4 * 2 = 8 espacios
+
+        // Array multidimensional
+        int[][][] numbers = new int[2][2][2];
+        int[][][][] numbers4 = new int[2][2][2][2];
+    }
+}
